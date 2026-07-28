5c2c5d4d83f81a3c460bb02342b3a0588e4b2f87
maryammamour125-beep <maryammamour125@gmail.com>
2026-07-28 20:17:04 +0100

    Create Appp.py

---
+++ Appp.py
@@ -0,0 +1,7 @@
+[Onboarding: Pick 1 of 3 Archetypes] 
+       ↓
+[Home Dashboard: View Today's "Scene" Quest & Prop Checklist]
+       ↓
+[Action: Complete Real-World Micro-Quest]
+       ↓
+[End of Day: 30-Second Reflection
